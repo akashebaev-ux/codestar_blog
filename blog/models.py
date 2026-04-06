9545f8077caf0270d4c6ec8828fab0ecf564a4ca
akashebaev-ux <akashebaev@gmail.com>
2026-04-06 22:44:09 +0200

    fix minor errors

--- blog/models.py
+++ blog/models.py
@@ -15,14 +15,15 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ['-created_on']
+
     def __str__(self):
         return f"The title of this post is {self.title} | written by {self.author}"
-    
 
-class Comment (models.Model):
+
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
     body = models.TextField()
@@ -31,5 +32,6 @@ class Comment (models.Model):
 
     class Meta:
         ordering = ['created_on']
+
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
